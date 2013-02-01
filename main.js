ab865461a65d4ca829ebea1bc5afabd906ba07b6
Sho Hashimoto <hashimoto@shokai.org>
2013-02-02 04:31:45 +0900

    use container

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 var Tv = function(selector){
   var self = this;
-  this.container = $(selector);
-  var iframe_css = {
-    width: "800px",
+  this.parent = $(selector);
+  this.container = $("<div>").css({
     height: "100%",
-    top: "0px",
-    left: "0px",
-    position: "absolute"
+    width: "100%",
+    margin: 0,
+    padding: 0,
+    overflow: "hidden"
+  });
+  this.parent.html(this.container);
+  var iframe_css = {
+    width: this.container.width(),
+    height: this.container.height(),
+    margin: 0,
+    padding: 0
   };
   this.iframe = $("<iframe>").css(iframe_css).attr({
     scrolling : "no"
@@ -18,20 +25,20 @@ var Tv = function(selector){
     return self.iframe.position().left;
   });
   this.__defineGetter__("top", function(){
-    return self.iframe.position().top;
+    return parseInt(self.iframe.css("margin-top"));
   });
   this.__defineSetter__("left", function(val){
-    self.iframe.position().left = val;
+    return parseInt(self.iframe.css("margin-left"));
   });
   this.__defineSetter__("top", function(val){
     if(val > 0) val = 0;
     self.iframe.height(self.__height - val);
-    self.iframe.css("top", val);
+    self.iframe.css("margin-top", val);
   });
   this.__defineSetter__("left", function(val){
     if(val > 0) val = 0;
     self.iframe.width(self.__width - val);
-    self.iframe.css("left", val);
+    self.iframe.css("margin-left", val);
   });
   this.load = function(url){
     console.log(url);
@@ -39,6 +46,9 @@ var Tv = function(selector){
     self.iframe.css(iframe_css);
     self.__height = self.iframe.height();
     self.__width = self.iframe.width();
+  };
+  this.scrollY = function(pix){
+    tv.top = tv.top - pix;
   };
 };
 
@@ -50,9 +60,9 @@ $(function(){
     tv.load($("#url").val());
   });
   $("#btn_down").click(function(){
-    tv.top = tv.top - 20;
+    tv.scrollY(20);
   });
   $("#btn_up").click(function(){
-    tv.top = tv.top + 20;
+    tv.scrollY(-20);
   });
 });
